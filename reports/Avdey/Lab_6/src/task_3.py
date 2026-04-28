867f95bd9f10b4bcf6d55909f50cdcdd76a0e72d
dzm1trok <145262377+dzm1trok@users.noreply.github.com>
2026-04-28 14:40:41 +0300

    feat: lab2

--- reports/Avdey/Lab_6/src/task_3.py
+++
@@ -1,6 +0,0 @@
-def repeat(pattern, count):
-    if pattern is None:
-        raise TypeError("Шаблон должен быть строкой.")
-    if count < 0:
-        raise ValueError("Число должно быть неотрицательным.")
-    return pattern * count
